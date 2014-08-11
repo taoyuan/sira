711bcf0f9e449cece9fe20809f5bd4b7790a9a5b
taoyuan <torworx@gmail.com>
2014-08-11 11:35:58 +0800

    model type support string format

--- test/fixtures/base-app/models/car.def.js
+++ test/fixtures/base-app/models/car.def.js
@@ -1,23 +1,21 @@
-module.exports = function (t) {
-    return {
-        "curd": true,
-        "properties": {
-            "make": {
-                "type": String,
-                "required": true
-            },
-            "model": {
-                "type": String,
-                "required": true
-            },
-            "desc": t.Text
+module.exports = {
+    "curd": true,
+    "properties": {
+        "make": {
+            "type": "String",
+            "required": true
         },
-        "relations": {
-            "dealer": {
-                "type": "belongsTo",
-                "model": "Dealership",
-                "foreignKey": "dealerId"
-            }
+        "model": {
+            "type": "String",
+            "required": true
+        },
+        "desc": "Text"
+    },
+    "relations": {
+        "dealer": {
+            "type": "belongsTo",
+            "model": "Dealership",
+            "foreignKey": "dealerId"
         }
     }
 };
